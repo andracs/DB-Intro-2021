84f0b8779ac5fd63b59354c31ed1d472d8742e0b
András Ács <5522871+andracs@users.noreply.github.com>
2021-02-01 22:13:25 +0100

    Initial commit

---
+++ src/SelectEksempel.java
@@ -0,0 +1,64 @@
+package net.sqlitetutorial;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class SelectEksempel {
+
+    /**
+     * Connect to the test.db database
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C:/Users/András Ács Pedersen/IdeaProjects/DB1/src/Opskrifter";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    
+    /**
+     * select all rows in the warehouses table
+     */
+    public void selectAll(){
+        String sql = "SELECT * FROM opskrifter";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  ". " +
+                                   rs.getString("navn") + "\n" +
+                                   rs.getString("opskrift") + "\n" +
+                                   rs.getString("ingredienser") + "\n\n"
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        net.sqlitetutorial.SelectEksempel app = new net.sqlitetutorial.SelectEksempel();
+        app.selectAll();
+    }
+
+}
