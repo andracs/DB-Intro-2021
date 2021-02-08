23e080db6106888d2ff98acdb083dea52d380734
András Ács <5522871+andracs@users.noreply.github.com>
2021-02-08 14:30:47 +0100

    Rettelser under forelæsnignen

--- src/SelectEksempel.java
+++ src/SelectEksempel.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 /**
  *
@@ -18,7 +19,7 @@ public class SelectEksempel {
      */
     private Connection connect() {
         // SQLite connection string
-        String url = "jdbc:sqlite:C:/Users/András Ács Pedersen/IdeaProjects/DB1/src/Opskrifter.db";
+        String url = "jdbc:sqlite:Opskrifter.db";
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
@@ -51,6 +52,34 @@ public class SelectEksempel {
             System.out.println(e.getMessage());
         }
     }
+
+    /**
+     * select all rows in the warehouses table
+     */
+    public void selectOne(){
+        String brugerInput;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Indtast et søgeord: ");
+        brugerInput = scanner.nextLine();
+
+        String sql = "SELECT * FROM opskrifter WHERE navn ='" + brugerInput + "'";
+        // Eksempel på LIKE (slet kommentartegnet og søg på la --> Du vil nu finde både Gulash og Lasagne, fordi de indeholde "la")
+        // sql = "SELECT * FROM opskrifter WHERE navn LIKE '%" + brugerInput + "%'";
+
+        System.out.println(sql);
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getString("navn"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     
    
     /**
@@ -58,7 +87,7 @@ public class SelectEksempel {
      */
     public static void main(String[] args) {
         net.sqlitetutorial.SelectEksempel app = new net.sqlitetutorial.SelectEksempel();
-        app.selectAll();
+        app.selectOne();
     }
 
 }
